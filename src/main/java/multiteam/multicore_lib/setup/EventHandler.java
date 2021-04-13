e7738bbb0dbd8a94242ea64fc521d11e1381997f
LazTheArtist <kingofcreepersyt@gmail.com>
2021-04-13 23:27:58 +0200

    Initial push, i guess

---
+++ src/main/java/multiteam/multicore_lib/setup/EventHandler.java
@@ -0,0 +1,71 @@
+package multiteam.multicore_lib.setup;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import multiteam.multicore_lib.MultiCoreLib;
+import net.minecraft.client.Minecraft;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.text.ITextProperties;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.RenderTooltipEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.List;
+
+@Mod.EventBusSubscriber(modid = MultiCoreLib.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class EventHandler {
+    @SubscribeEvent
+    @OnlyIn(Dist.CLIENT)
+    public static void renderTooltip(RenderTooltipEvent.PostText event) {
+        ItemStack stack = event.getStack();
+
+        RenderSystem.pushMatrix();
+        RenderSystem.translatef(event.getX(), event.getY() + 12, 500);
+        RenderSystem.scalef(0.5f, 0.5f, 1.0f);
+        Minecraft mc = Minecraft.getInstance();
+        List<? extends ITextProperties> tooltip = event.getLines();
+
+        CompoundNBT nbtTagCompound = stack.getTag();
+
+        if(nbtTagCompound != null){
+
+            if(nbtTagCompound.getBoolean("hasTooltipItem")){
+                for (int i = 0; i < nbtTagCompound.getIntArray("itemsToRender").length; i++){
+                    ItemStack displayStack = new ItemStack(Item.byId(nbtTagCompound.getIntArray("itemsToRender")[i]));
+                    if(displayStack != null){
+                        mc.getItemRenderer().renderGuiItem(displayStack, i * 17, nbtTagCompound.getInt("lineToRender") + 20);
+                    }
+                }
+            }
+
+            if(nbtTagCompound.getBoolean("hasItemBar")){
+                int barLength = nbtTagCompound.getInt("barLenght");
+                int barFillAmount = nbtTagCompound.getInt("barFillAmountDisplay");
+                Item[] itemsToRender = new Item[nbtTagCompound.getInt("barLenght")];
+
+                for (int i = 0; i < barLength; i++) {
+                    if(barFillAmount == 0){
+                        itemsToRender[i] = Item.byId(nbtTagCompound.getInt("barItemEmpty"));
+                    }else if(i < barFillAmount){
+                        itemsToRender[i] = Item.byId(nbtTagCompound.getInt("barItem"));
+                    }else{
+                        itemsToRender[i] = Item.byId(nbtTagCompound.getInt("barItemEmpty"));
+                    }
+                }
+
+                for (int i = 0; i < itemsToRender.length; i++){
+                    ItemStack displayStack = new ItemStack(itemsToRender[i]);
+                    if(displayStack != null){
+                        mc.getItemRenderer().renderGuiItem(displayStack, i * 17, nbtTagCompound.getInt("lineToRender") + 20);
+                    }
+                }
+            }
+
+        }
+
+        RenderSystem.popMatrix();
+    }
+}
---
+++ src/main/java/multiteam/multicore_lib/setup/Registration.java
@@ -0,0 +1,8 @@
+package multiteam.multicore_lib.setup;
+
+public class Registration {
+
+    public static void register() {
+        //Dummy Class so far. probs gon be stuff here.
+    }
+}

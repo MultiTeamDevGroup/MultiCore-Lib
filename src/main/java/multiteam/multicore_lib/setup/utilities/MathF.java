fe0ab7f2acda3cf0525a15aa04c40cbe4fabf580
LazTheArtist <kingofcreepersyt@gmail.com>
2021-04-26 18:05:54 +0200

    0.0.1.2

--- src/main/java/multiteam/multicore_lib/setup/utilities/MathF.java
+++ src/main/java/multiteam/multicore_lib/setup/utilities/MathF.java
@@ -1,7 +1,7 @@
 package multiteam.multicore_lib.setup.utilities;
 
 public class MathF { //Why MathF? Because in unity, there was a class with Mathematical functions, and i grown quiet fond of this name. In the honor of my C# past, you shall now be named MathF!
-    
+
     public float rescaleValues(float minFrom, float maxFrom, float minTo, float maxTo, float valueToScale){
         float OldRange = (maxFrom - minFrom);
         float NewRange = (maxTo - minTo);
